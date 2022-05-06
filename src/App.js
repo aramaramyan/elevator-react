93115ce1943eadcf8790fa6c342923750bed3330
aramaramyan <aramaramyan1997@mail.ru>
2022-05-06 09:43:18 +0400

    feat: crete Building component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Building from "./Building/Building";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-
+      <Building />
     </div>
   );
 }
---
+++ src/Building/Building.jsx
@@ -0,0 +1,11 @@
+import "./Building.css";
+
+export default function Building() {
+  return (
+    <>
+      <div className="building" />
+      <div className="black_bg" />
+      <div className="elevator" />
+    </>
+  );
+}
