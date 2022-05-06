f6ab0f366ad1822175762ac16f18bfd8e7748acb
aramaramyan <aramaramyan1997@mail.ru>
2022-05-06 10:51:18 +0400

    feat: add button styles to RemoteControl component

--- src/components/RemoteControl/RemoteControl.jsx
+++ src/components/RemoteControl/RemoteControl.jsx
@@ -4,8 +4,11 @@ const buttons = Array.from(Array(12).keys());
 
 export default function RemoteControl() {
   return (
-    <div className="buttons_wrapper">
-      {buttons.map(button => <div key={button} className="button" />)}
+    <div className="remote_control">
+      <div className="buttons_wrapper">
+        {buttons.map(button => <div key={button} className="button" />)}
+
+      </div>
     </div>
   );
 }
